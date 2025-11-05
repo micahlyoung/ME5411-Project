ff9e8dda4524078a265f713416f92acc577a697f
ManBan Mak <wenbinmai746@gmail.com>
2025-11-05 17:57:50 +0800

    Added training code and outputs for CNN

---
+++ CNN_train.m
@@ -0,0 +1,85 @@
+clc; 
+clear; 
+%% load data 
+path = './dataset_2025'; 
+
+imds = imageDatastore(path, "IncludeSubfolders", true, ...
+    'LabelSource', 'foldernames'); 
+
+[imdsTrain, imdsValandTest] = splitEachLabel(imds, 0.75, 'randomized'); 
+[imdsVal, imdsTest] = splitEachLabel(imds, 0.60, 'randomized'); 
+
+%% visualization
+% figure; 
+% perm = randperm(numel(imds.Files), 20); 
+% for i = 1:20
+%     subplot(4,5,i);
+%     imshow(imds.Files{perm(i)});
+%     title(char(imds.Labels(perm(i)))); 
+%     drawnow;
+% end
+
+%% preprocessing
+inputSize = [32 32 1]; 
+
+imageAugmenter = imageDataAugmenter( ...
+    'RandRotation',[-10,10], ...
+    'RandXTranslation',[-3 3], ...
+    'RandYTranslation',[-3 3]);
+
+augimdsTrain = augmentedImageDatastore(inputSize, imdsTrain, ...
+    'DataAugmentation', imageAugmenter);
+augimdsVal = augmentedImageDatastore(inputSize, imdsVal);
+augimdsTest = augmentedImageDatastore(inputSize, imdsTest);
+
+%% define network
+numClasses = numel(categories(imdsTrain.Labels));
+layers = [
+    imageInputLayer(inputSize, 'Name', 'input')
+    convolution2dLayer(3, 4, 'Padding','same', 'Name', 'conv1')
+    batchNormalizationLayer('Name', 'bn1')
+    reluLayer('Name', 'relu1')
+    maxPooling2dLayer(2, 'Stride', 2, 'Name', 'maxpool1')
+
+    convolution2dLayer(3, 16, 'Padding','same', 'Name', 'conv2')
+    batchNormalizationLayer('Name', 'bn2')
+    reluLayer('Name', 'relu2')
+    maxPooling2dLayer(2, 'Stride', 2, 'Name', 'maxpool2')
+
+    convolution2dLayer(3, 32, 'Padding','same', 'Name', 'conv3')
+    batchNormalizationLayer('Name', 'bn3')
+    reluLayer('Name', 'relu3')
+
+    fullyConnectedLayer(numClasses, 'Name', 'fc')
+    softmaxLayer('Name', 'softmax')
+    classificationLayer('Name', 'classoutput')
+];
+
+analyzeNetwork(layers);
+%% train
+options = trainingOptions('adam', ...
+    'InitialLearnRate',1e-4, ...
+    'MaxEpochs',50, ...
+    'MiniBatchSize', 128, ...           
+    'Shuffle','every-epoch', ...       
+    'ValidationData',augimdsVal, ...
+    'ValidationFrequency',floor(numel(augimdsTrain.Files)*128), ...
+    'Verbose',true, ...
+    'Plots','none', ...
+    'OutputFcn', @CNN_visualization.plotTrainingProgress); 
+[net, trainInfo] = trainNetwork(augimdsTrain, layers, options);
+
+%% 
+save('CNN_output/trained_net_cnn.mat', 'net');
+save('CNN_output/training_info_cnn.mat', 'trainInfo');
+
+%% scoring
+[YPred, scores] = classify(net, augimdsTest);
+YVal = imdsTest.Labels;
+
+accuracy = sum(YPred == YVal) / numel(YVal);
+fprintf('Validation accuracy: %.2f%%\n', accuracy * 100);
+
+figure;
+confusionchart(YVal, YPred);
+title('Confusion Matrix for Validation Data');
---
+++ CNN_visualization.m
@@ -0,0 +1,54 @@
+classdef CNN_visualization
+    methods (Static)
+        function stop = plotTrainingProgress(info)
+            persistent plotHandles; 
+            
+            cmap = parula(10); 
+            stop = false;
+
+            if info.State == "start"
+                figure('Units','normalized','Position',[0.2 0.2 0.6 0.5]);
+                
+                ax1 = subplot(1, 2, 1);
+                
+                plotHandles.trainAcc = animatedline('Color', cmap(2,:), 'Marker', '.', 'LineStyle', '-', 'LineWidth', 1.5);
+                plotHandles.valAcc = animatedline('Color', cmap(8,:), 'Marker', '*', 'LineStyle', '-', 'LineWidth', 1.5);
+                
+                xlabel(ax1, "Iteration");
+                ylabel(ax1, "Accuracy");
+                title(ax1, 'Accuracy Progress');
+                legend(ax1, 'Training', 'Validation');
+                grid(ax1, 'on');
+
+                ax2 = subplot(1, 2, 2);
+
+                plotHandles.trainLoss = animatedline('Color', cmap(2,:), 'Marker', '.', 'LineStyle', '-', 'LineWidth', 1.5);
+                plotHandles.valLoss = animatedline('Color', cmap(8,:), 'Marker', '*', 'LineStyle', '-', 'LineWidth', 1.5);
+
+                xlabel(ax2, "Iteration");
+                ylabel(ax2, "Loss");
+                title(ax2, 'Loss Progress');
+                legend(ax2, 'Training', 'Validation'); 
+                grid(ax2, 'on');
+            end
+
+            if info.State == "iteration"
+                if ~isempty(info.TrainingAccuracy)
+                    addpoints(plotHandles.trainAcc, info.Iteration, info.TrainingAccuracy);
+                end
+                if ~isempty(info.ValidationAccuracy)
+                    addpoints(plotHandles.valAcc, info.Iteration, info.ValidationAccuracy);
+                end
+
+                if ~isempty(info.TrainingLoss)
+                    addpoints(plotHandles.trainLoss, info.Iteration, info.TrainingLoss);
+                end
+
+                if isfield(info, 'ValidationLoss') && ~isempty(info.ValidationLoss)
+                    addpoints(plotHandles.valLoss, info.Iteration, info.ValidationLoss);
+                end
+                drawnow limitrate;
+            end
+        end
+    end
+end
